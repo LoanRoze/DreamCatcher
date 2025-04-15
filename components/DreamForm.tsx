e69c8e8403136bad118a597dc5ba43d9b4d54b01
Muffin <loan.roze@efrei.net>
2025-04-15 21:23:17 +0200

    commit

--- components/DreamForm.tsx
+++ components/DreamForm.tsx
@@ -1,38 +1,62 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native';
-import { TextInput, Button, Checkbox } from 'react-native-paper';
+import { StyleSheet, Dimensions, ScrollView } from 'react-native';
+import { View, Text } from './Themed';
+import { TextInput, Button, Checkbox, List  } from 'react-native-paper';
 import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import RNDateTimePicker from 'react-datetime-picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import 'react-datetime-picker/dist/DateTimePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import 'react-clock/dist/Clock.css';
+import Slider from '@react-native-community/slider';
+
+
 
 const { width } = Dimensions.get('window');
 
+const todayDate = new Date();
+const month = todayDate.getMonth() + 1;
+const year = todayDate.getFullYear();
+const day = todayDate.getDate();
+const formattedDate =  `${year}-${month}-${day}`;
+
 export default function DreamForm() {
   const [dreamText, setDreamText] = useState('');
-  const [hashtag1, setHashtag1] = useState('');
-  const [hashtag2, setHashtag2] = useState('');
-  const [hashtag3, setHashtag3] = useState('');
-  const [isLucidDream, setIsLucidDream] = useState(false);
   const [date, setDate] = useState('')
+  const [hashtags, setHashtags] = useState<string[]>([])
+  const [inputCountHashtags, setInputCountHashtags] = useState(0); 
+  const [hour, setHour] = useState('');
+  const [hourDateType, setHourDateType] = useState('')
+  const [dreamType, setDreamType] = useState('');
+  const [emotionalState, setEmotionalState] = useState('');
+  const [peopleList, setPeopleList] = useState<string[]>([]);
+  const [inputCountPeople, setInputCountPeople] = useState(0);
+  const [dreamLocation, setDreamLocation] = useState('');
+  const [emotionalIntensity, setEmotionalIntensity] = useState(5); 
+  const [dreamClarity, setDreamClarity] = useState(5); 
+  const [sleepQuality, setSleepQuality] = useState(5); 
+  const [dreamSignification, setDreamSignification] = useState('');
+  const [dreamMood, setDreamMood] = useState('');
+  const [expanded, setExpanded] = useState({});
 
-  const today = new Date();
-  const month = today.getMonth() + 1;
-  const year = today.getFullYear();
-  const day = today.getDate();
-  const formattedDate =  `${year}-${month}-${day}`;
+  
+  const toggleExpand = (key : any) => {
+    setExpanded((prev) => ({ ...prev, [key]: !prev[key] }));
+  };
 
-  const findHashtagIdByLabel = async (hashtag) => {
+
+
+
+  const findHashtagIdByLabel = async (hashtag : any) => {
     try {
-      // Récupère les données des rêves stockées dans le AsyncStorage
       const existingDreams = await AsyncStorage.getItem('dreamFormDataArray');
       let dreamsData = existingDreams ? JSON.parse(existingDreams) : [];
   
-      // Parcours tous les rêves pour trouver un hashtag existant
       for (let dream of dreamsData) {
         for (let hashtagKey in dream.hashtags) {
-          const hashtagStored = dream.hashtags[hashtagKey]; // Récupère l'objet du hashtag stocké
+          const hashtagStored = dream.hashtags[hashtagKey]; 
           console.log(hashtag, hashtagStored.label)
           if (hashtagStored.label === hashtag) {
-            // Si le hashtag est trouvé, renvoie son ID
             return hashtagStored.id;
           }
         }
@@ -47,116 +71,302 @@ export default function DreamForm() {
       return null;
     }
   };
-  
-  
 
+  const findPersonIdByLabel = async (person : any) => {
+    try {
+      const existingDreams = await AsyncStorage.getItem('dreamFormDataArray');
+      let dreamsData = existingDreams ? JSON.parse(existingDreams) : [];
+  
+      for (let dream of dreamsData) {
+        for (let personKey in dream.peopleList) {
+          const personStored = dream.hashtags[personKey]; 
+          console.log(person, personStored.label)
+          if (personStored.label === person) {
+            return personStored.id;
+          }
+        }
+      }
+  
+      const newId = `person-${Math.random().toString(36).substr(2, 9)}`;
+      return newId;
+  
+    } catch (error) {
+      console.error('Erreur lors de la gestion des personnes:', error);
+      return null;
+    }
+  };
+
+  const onChangeHour = (newHour : any) => {
+    if (newHour) {
+      const newHourHours = newHour.getHours()
+      const newHourMinutes = newHour.getMinutes()
+      const formattedTime = `${newHourHours.toString().padStart(2, '0')}:${newHourMinutes.toString().padStart(2, '0')}`;
+      const selectedHour = formattedTime || hour; // Si aucune date n'est sélectionnée, garde la date actuelle
+      setHour(selectedHour); // Met à jour la date
+      setHourDateType(newHour)
+    }
+  };
+
+  const updateHashtag = (index: number, value: string) => {
+    const newHashtags = [...hashtags];
+    newHashtags[index] = value;
+    setHashtags(newHashtags);
+  };
+  const updatePeople = (index: number, value: string) => {
+    const newPeople = [...peopleList];
+    newPeople[index] = value;
+    setPeopleList(newPeople);
+  };
+
+  
   const handleDreamSubmission = async () => {
 
     try {
-      // Récupérer le tableau actuel depuis AsyncStorage
       const existingData = await AsyncStorage.getItem('dreamFormDataArray');
       const formDataArray = existingData ? JSON.parse(existingData) : [];
+      
+      let formattedHashtags
+      formattedHashtags = await Promise.all(
+        hashtags.map(async (label) => ({
+            id: await findHashtagIdByLabel(label),
+            label: label
+        }))
+      );
+      
+      let formattedPeopleList
+      formattedPeopleList = await Promise.all(
+        hashtags.map(async (label) => ({
+            id: await findHashtagIdByLabel(label),
+            label: label
+        }))
+      );
 
-      // Trouver les IDs des hashtags
-      const hashtag1Id = await findHashtagIdByLabel(hashtag1);
-      const hashtag2Id = await findHashtagIdByLabel(hashtag2);
-      const hashtag3Id = await findHashtagIdByLabel(hashtag3);
-
-      // Ajouter le nouveau formulaire au tableau
       formDataArray.push({
+        id: Math.random().toString(36).substr(2, 9), // Générer un ID unique
         dreamText: dreamText,
-        isLucidDream: isLucidDream,
-        todayDate: new Date(),
-        hashtags: [
-          { id: hashtag1Id, label: hashtag1 },
-          { id: hashtag2Id, label: hashtag2 },
-          { id: hashtag3Id, label: hashtag3 },
-        ],
+        date: date,
+        hashtags: formattedHashtags, 
+        hour: hour,
+        dreamType: dreamType,
+        emotionalState: emotionalState,
+        peopleList: formattedPeopleList,
+        dreamLocation: dreamLocation,
+        emotionalIntensity: emotionalIntensity,
+        dreamClarity: dreamClarity,
+        sleepQuality: sleepQuality,
+        dreamSignification: dreamSignification,
+        dreamMood: dreamMood,
       });
+      console.log(formDataArray)
 
       // Sauvegarder le tableau mis à jour dans AsyncStorage
       await AsyncStorage.setItem('dreamFormDataArray', JSON.stringify(formDataArray));
 
       // Réinitialiser les champs du formulaire
-      setDreamText('');
-      setIsLucidDream(false);
-      setHashtag1('');
-      setHashtag2('');
-      setHashtag3('');
+      setDreamText('')
+      setDate('')
+      setHashtags([])
+      setInputCountHashtags(0)
+      setHour('')
+      setDreamType('')
+      setEmotionalState('')
+      setPeopleList([])
+      setInputCountPeople(0)
+      setDreamLocation('')
+      setEmotionalIntensity(5)
+      setDreamClarity(5)
+      setSleepQuality(5)
+      setDreamSignification('')
+      setDreamMood('')
+      setExpanded({})
 
     } catch (error) {
       console.error('Erreur lors de la sauvegarde des données:', error);
     }
 
-    setDreamText('');
-    setIsLucidDream(false);
   };
   
   return (
     <View style={styles.container}>
+      <ScrollView>
 
-      <TextInput
-        label="Rêve"
-        value={dreamText}
-        onChangeText={(text) => setDreamText(text)}
-        mode="outlined"
-        multiline
-        numberOfLines={6}
-        style={[styles.input, { width: width * 0.8, alignSelf: 'center' }]}
-      />
+        {/*Champs Obligatoires*/}
 
-<TextInput
-        label="Hashtag 1"
-        value={hashtag1}
-        onChangeText={(hashtag1) => setHashtag1(hashtag1)}
-        mode="outlined"
-        style={[styles.input, { width: width * 0.8, alignSelf: 'center' }]}
-      />
-
-      <TextInput
-        label="Hashtag 2"
-        value={hashtag2}
-        onChangeText={(hashtag2) => setHashtag2(hashtag2)}
-        mode="outlined"
-        style={[styles.input, { width: width * 0.8, alignSelf: 'center' }]}
-      />
-
-      <TextInput
-        label="Hashtag 3"
-        value={hashtag3}
-        onChangeText={(hashtag3) => setHashtag3(hashtag3)}
-        mode="outlined"
-        style={[styles.input, { width: width * 0.8, alignSelf: 'center' }]}
-      />
-
-      <Calendar
-        style={{
-          borderWidth: 1,
-          borderColor: 'gray',
-          height: 350,
-          width: 200,
-          alignSelf: 'center',
-        }}
-        // Specify the current date
-        current = {formattedDate}
-        // Callback that gets called when the user selects a day
-        onDayPress={ day => {
-          setDate(day.dateString)
-        }}
-        markedDates={{
-          [date]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
-        }}
-      />
-
-      <View style={styles.checkboxContainer}>
-        <Checkbox.Item
-          label="Rêve Lucide"
-          status={isLucidDream ? 'checked' : 'unchecked'}
-          onPress={() => setIsLucidDream(!isLucidDream)}
+        <TextInput
+          label="Rêve"
+          value={dreamText}
+          onChangeText={(text) => setDreamText(text)}
+          mode="outlined"
+          multiline
+          numberOfLines={6}
+          style={[styles.input, { width: width * 0.8, alignSelf: 'center' }]}
         />
-      </View>
 
-      <Button mode="contained" onPress={handleDreamSubmission} style={styles.button}>
+        <Calendar
+          style={styles.calendar}
+          // Specify the current date
+          current = {formattedDate}
+          // Callback that gets called when the user selects a day
+          onDayPress={ day => {
+            setDate(day.dateString)
+          }}
+          markedDates={{
+            [date]: {selected: true, disableTouchEvent: true, selectedDotColor: 'orange'}
+          }}
+        />
+
+        <View style={styles.spacing} />
+        
+        <View style={styles.datetimePickerContainer}>
+          <RNDateTimePicker
+            value={hourDateType}
+            onChange={onChangeHour} 
+            disableClock={true} 
+            format="HH:mm" 
+            clearIcon={null} 
+            calendarIcon={null} 
+          />
+        </View>
+
+        <TextInput
+          label="Type du rêve"
+          value={dreamType}
+          onChangeText={(text) => setDreamType(text)}
+          mode="outlined"
+          style={[styles.input, { width: width * 0.8, alignSelf: 'center' }]}
+        />  
+
+
+
+        {/*Champs Optionnels*/}
+        <List.Accordion
+          style={styles.accordion}
+          title="Autres détails"
+          expanded={expanded.details}
+          onPress={() => toggleExpand('details')}
+          left={props => <List.Icon {...props} icon={expanded.details ? 'chevron-up' : 'chevron-down'} />}
+        >
+          <Button 
+            mode="contained" 
+            onPress={() => {
+              setInputCountHashtags(inputCountHashtags + 1);
+              setHashtags([...hashtags, ""]); // Ajoute un espace pour le nouvel input
+            }}
+            style={styles.addButton}
+          >
+            + Ajouter un hashtag
+          </Button>
+          {[...Array(inputCountHashtags)].map((_, index) => (
+            <TextInput
+              key={index}
+              label={`Hashtag ${index + 1}`}
+              value={hashtags[index]}
+              onChangeText={(text) => updateHashtag(index, text)}
+              mode="outlined"
+              style={[styles.input, { width: width * 0.8, alignSelf: 'center' }]}
+            />
+          ))}
+
+          <TextInput
+            label="Etat Emotional"
+            value={emotionalState}
+            onChangeText={(text) => setEmotionalState(text)}
+            mode="outlined"
+            style={[styles.input, { width: width * 0.8, alignSelf: 'center' }]}
+          />
+
+          <Button 
+            mode="contained" 
+            onPress={() => {
+              setInputCountPeople(inputCountPeople + 1);
+              setPeopleList([...peopleList, ""]); // Ajoute un espace pour le nouvel input
+            }}
+            style={styles.addButton}
+          >
+            + Ajouter une personne
+          </Button>
+          {[...Array(inputCountPeople)].map((_, index) => (
+            <TextInput
+              key={index}
+              label={`Personne ${index + 1}`}
+              value={peopleList[index]}
+              onChangeText={(text) => updatePeople(index, text)}
+              mode="outlined"
+              style={[styles.input, { width: width * 0.8, alignSelf: 'center' }]}
+            />
+          ))}
+
+          <TextInput
+            label="Lieu du Rêve"
+            value={dreamLocation}
+            onChangeText={(text) => setDreamLocation(text)}
+            mode="outlined"
+            style={[styles.input, { width: width * 0.8, alignSelf: 'center' }]}
+          />
+
+          <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 10 }}>
+            Intensité émotionelle du rêve :
+          </Text>
+          <Slider
+            style={{ width: 200, height: 40 }}
+            minimumValue={0}
+            maximumValue={10}
+            value={emotionalIntensity}
+            minimumTrackTintColor="#A9A9A9" // Gris foncé
+            maximumTrackTintColor="#D3D3D3" // Gris clair
+            onValueChange={(number) => setEmotionalIntensity(number)}
+          />
+
+          <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 10 }}>
+            Clareté du rêve :
+          </Text>
+          <Slider
+            style={{ width: 200, height: 40 }}
+            minimumValue={0}
+            maximumValue={10}
+            value={dreamClarity}
+            minimumTrackTintColor="#A9A9A9" // Gris foncé
+            maximumTrackTintColor="#D3D3D3" // Gris clair
+            onValueChange={(number) => setDreamClarity(number)}
+          />
+
+          <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 10 }}>
+            Qualité du sommeil :
+          </Text>
+          <Slider
+            style={{ width: 200, height: 40 }}
+            minimumValue={0}
+            maximumValue={10}
+            value={sleepQuality}
+            minimumTrackTintColor="#A9A9A9" // Gris foncé
+            maximumTrackTintColor="#D3D3D3" // Gris clair
+            onValueChange={(number) => setSleepQuality(number)}
+          />
+
+          <TextInput
+            label="Signification du rêve"
+            value={dreamSignification}
+            onChangeText={(text) => setDreamSignification(text)}
+            mode="outlined"
+            style={[styles.input, { width: width * 0.8, alignSelf: 'center' }]}
+          />
+
+          <TextInput
+            label="Tonalité du rêve"
+            value={dreamMood}
+            onChangeText={(text) => setDreamMood(text)}
+            mode="outlined"
+            style={[styles.input, { width: width * 0.8, alignSelf: 'center' }]}
+          />
+        </List.Accordion>
+      </ScrollView>
+
+      <Button 
+        mode="contained" 
+        onPress={handleDreamSubmission} 
+        style={styles.button}
+        disabled={!dreamText.trim() || !date.trim() || !hour.trim() || !dreamType.trim()}
+      >
         Soumettre
       </Button>
 
@@ -164,12 +374,42 @@ export default function DreamForm() {
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
     padding: 16,
+    backgroundColor: '#f5f5f5',
+    width: '90%',
+    height: "80%",
   },
   input: {
     marginBottom: 16,
+    backgroundColor: 'white',
+    paddingLeft: 0,
+
+  },
+  calendar: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    height: 350,
+    width: 200,
+    alignSelf: 'center',
+  },
+  datetimePickerContainer: {
+    alignSelf: 'center',
+    marginBottom: 16,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    alignSelf: 'center',
+  },
+  slider: {
+    width: 200,
+    height: 40,
+    alignSelf: 'center',
   },
   checkboxContainer: {
     flexDirection: 'row',
@@ -177,6 +417,27 @@ const styles = StyleSheet.create({
     marginBottom: 16,
   },
   button: {
-    marginTop: 8,
+    marginTop: 16,
+    alignSelf: 'center',
+  },
+  hashtagButton: {
+    alignSelf: 'center',
+    marginBottom: 8,
+  },
+  spacing: {
+    height: 16, // Ajustez cette valeur pour l'espacement souhaité
+    backgroundColor: '#f5f5f5',
+  },
+  accordion: {
+    backgroundColor: '#e0e0e0 !important', // Couleur de fond pour ressembler à un bouton
+    borderRadius: 8,
+    marginVertical: 8,
+    elevation: 2,
+  },
+  addButton: {
+    marginTop: 16,
+    paddingLeft: 0,
+    alignSelf: 'center',
+    marginBottom: 8,
   },
 });
--- components/DreamList.tsx
+++ components/DreamList.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { TextInput } from 'react-native-paper';
+import { View, Text, StyleSheet, FlatList, ScrollView } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useFocusEffect } from 'expo-router';
@@ -11,25 +11,41 @@ export default function DreamList() {
     const [searchText, setSearchText] = useState('');
     const [filteredDreams, setFilteredDreams] = useState([]);
 
-    const findHashtagIdByLabel = async (hashtag) => {
-        const existingDreams = await AsyncStorage.getItem('dreamFormDataArray');
-        let dreamsData = existingDreams ? JSON.parse(existingDreams) : [];
-    
-        // Parcours tous les rêves pour trouver un hashtag existant
-        for (let dream of dreamsData) {
-          for (let hashtagKey in dream.hashtags) {
-            const hashtagStored = dream.hashtags[hashtagKey]; // Récupère l'objet du hashtag stocké
-            console.log(hashtag, hashtagStored.label)
-            if (hashtagStored.label === hashtag) {
-              // Si le hashtag est trouvé, renvoie son ID
-              return hashtagStored.id;
+    const findHashtagIdByLabel = async (hashtag: any) => {
+        try {
+            const existingDreams = await AsyncStorage.getItem('dreamFormDataArray');
+            let dreamsData = existingDreams ? JSON.parse(existingDreams) : [];
+
+            for (let dream of dreamsData) {
+                for (let hashtagKey in dream.hashtags) {
+                    const hashtagStored = dream.hashtags[hashtagKey];
+                    console.log(hashtag, hashtagStored.label)
+                    if (hashtagStored.label === hashtag) {
+                        return hashtagStored.id;
+                    }
+                }
             }
-          }
+
+            const newId = `hashtag-${Math.random().toString(36).substr(2, 9)}`;
+            return newId;
+
+        } catch (error) {
+            console.error('Erreur lors de la gestion des hashtags:', error);
+            return null;
         }
-        return null
+    };
+
+    const deleteDream = ({ dream }) => {
+        try {
+            const updatedDreams = dreams.filter((item) => item.id !== dream.id);
+            AsyncStorage.setItem('dreamFormDataArray', JSON.stringify(updatedDreams));
+            setDreams(updatedDreams);
+
+        } catch (error) {
+            console.error('Erreur lors de la suppression du rêve:', error);
+        }
     }
 
-    // Ce useEffect est exécuté à l'instanciation du composant pour charger la liste initiale
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -44,7 +60,7 @@ export default function DreamList() {
         fetchData();
     }, []);
 
-    useEffect( () => {
+    useEffect(() => {
         const fetchFilteredDreams = async () => {
             if (!searchText) {
                 setFilteredDreams(dreams)
@@ -89,8 +105,9 @@ export default function DreamList() {
         }
             , [])
     );
+
     return (
-        <View>
+        <View style={styles.container}>
             <TextInput
                 label="Rechercher un rêve avec un hashtag"
                 value={searchText}
@@ -99,34 +116,133 @@ export default function DreamList() {
             />
 
             <Text style={styles.title}>Liste des Rêves :</Text>
-            
-            {filteredDreams.map((dream, index) => (
-        
-                <Text key={index} style={styles.dreamText}>
-                    {dream.dreamText} - {dream.isLucidDream ? 'Lucide' : 'Non Lucide'} - {dream.todayDate}
-                    <br />
-                    Hashtags:
-                    <br />
-                    1. {dream.hashtags[0].id} - {dream.hashtags[0].label}
-                    <br />
-                    2. {dream.hashtags[1].id} - {dream.hashtags[1].label}
-                    <br />
-                    3. {dream.hashtags[2].id} - {dream.hashtags[2].label}
-                </Text>
-            ))}
+
+            <ScrollView style={[styles.scrollContainer]}>
+                {filteredDreams.map((dream, index) => (
+                    <View style={styles.card}>
+                        <Text style={styles.title}>{dream.dreamText} 🌙</Text>
+                        <Text style={styles.text}>{dream.dreamType}</Text>
+
+                        {dream.dreamLocation && (
+                            <View style={styles.row}>
+                                <Text style={styles.label}>Lieu :</Text>
+                                <Text style={styles.value}>{dream.dreamLocation}</Text>
+                            </View>
+                        )}
+
+                        {dream.emotionalState && (
+                            <View style={styles.row}>
+                                <Text style={styles.label}>État émotionnel :</Text>
+                                <Text style={styles.value}>{dream.emotionalState}</Text>
+                            </View>
+                        )}
+
+                        <FlatList
+                            data={dream.hashtags}
+                            horizontal
+                            keyExtractor={(item, index) => index.toString()}
+                            renderItem={({ item }) => <Text style={styles.tag}>#{item.label}</Text>}
+                        />
+
+                        <FlatList
+                            data={dream.peopleList}
+                            horizontal
+                            keyExtractor={(item, index) => index.toString()}
+                            renderItem={({ item }) => <Text style={styles.peopleTag}>👤{item}</Text>}
+                        />
+
+                        <Text style={styles.footer}>🕒 {dream.hour} | 📅 {dream.date}</Text>
+                        <Button
+                            mode="contained"
+                            onPress={() => deleteDream({ dream })}
+                            icon="delete"
+                            style={styles.buttonDelete}
+                        >
+                        </Button>
+                    </View>
+                ))}
+            </ScrollView>
         </View>
     );
 }
 
 
 const styles = StyleSheet.create({
-    title: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginBottom: 8,
-    },
     dreamText: {
         fontSize: 16,
         marginBottom: 4,
     },
-});
+    card: {
+        backgroundColor: "#fff",
+        padding: 20,
+        borderRadius: 15,
+        shadowColor: "#000",
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
+        elevation: 5,
+        margin: 10,
+        width: "90%",
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginBottom: 5,
+        color: "#333",
+    },
+    text: {
+        fontSize: 16,
+        color: "#444",
+        marginBottom: 10,
+    },
+    row: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginBottom: 5,
+    },
+    label: {
+        fontWeight: "bold",
+        color: "#555",
+        marginRight: 5,
+    },
+    value: {
+        color: "#222",
+    },
+    tag: {
+        backgroundColor: "#ddd",
+        color: "#333",
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        borderRadius: 15,
+        marginRight: 5,
+        fontSize: 12,
+    },
+    peopleTag: {
+        backgroundColor: "#87CEFA",
+        color: "#fff",
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        borderRadius: 15,
+        marginRight: 5,
+        fontSize: 12,
+    },
+    footer: {
+        fontSize: 12,
+        color: "#888",
+        marginTop: 10,
+        textAlign: "right",
+    },
+    container: {
+        width: "80%",
+        height: "40%",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    scrollContainer: {
+        padding: 20,
+        backgroundColor: "#f5f5f5",
+        height: "70%",
+        display: 'flex',
+    }
+})
+    ;
